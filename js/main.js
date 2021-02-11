5f7448f35aa06d71a567df0b4a3795c60a9c0dcf
Marcus Graci <mpgraci@gmail.com>
2021-02-11 15:52:57 -0500

    added pages and sort functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const tbody = document.getElementById('table-body');
 const filters = document.getElementById('filter-list');
+const pagination = document.getElementsByClassName('pagination');
+const nextBtn = document.getElementById('next_page');
+const prevBtn = document.getElementById('prev_page');
+const results = document.getElementById('num-results');
+const selectedSort = document.getElementById('selected-sort');
 
 const url = "https://free-to-play-games-database.p.rapidapi.com/api/games";
 const headers = { 
@@ -10,6 +15,27 @@ const headers = {
 	}}
 
 let gameList = {};
+let currentPage = 1;
+let itemsPerPage = 5;
+
+//pagination
+function prevPage(){
+	if(currentPage > 1){
+		currentPage--;
+		createGameList(currentPage);
+	}
+}
+
+function nextPage(){	
+	if(currentPage < numPages()){
+		currentPage++;
+		createGameList(currentPage);
+	}
+}
+
+function numPages(){
+	return Math.ceil(gameList.length / itemsPerPage);
+}
 
 //fetch
 function fetchData(url) {
@@ -18,17 +44,15 @@ function fetchData(url) {
 			.catch(err => console.error(err));	
 }
 
-fetchData(url)	
-		.then(data => {
-			gameList = data;	
-			createGameList();
-			createFilterList();			
-});	
+//generates game list
+function createGameList(page){
+	tbody.innerHTML = "";
+	
+	results.innerHTML = gameList.length + " free game(s)"
+	
+	for(i = (page-1) * itemsPerPage; i < (page * itemsPerPage) && i < gameList.length; i++){
+		let num = i+1;	
 
-//generates HTML
-function createGameList(){
-	for(i = 0; i < 20; i++){
-		let num = i+1;				
 		tbody.innerHTML += `			
 			<tr>
 				<td class="number text-center">` + num + `</td>
@@ -39,9 +63,22 @@ function createGameList(){
 			</tr>
 			`
 	};
+
+	//changes pagination styles
+	if(page == 1){
+		prevBtn.className = 'disabled';
+	} else {
+		prevBtn.className = 'active';
+	}
+
+	if(page == numPages()){
+		nextBtn.className = 'disabled';
+	} else {
+		nextBtn.className = 'active';
+	}
 }
 
-//creates filter list based on entire game list from API
+//creates filter list 
 function createFilterList(){	
 	let distinct = [];	
 	let unique = [];
@@ -49,22 +86,63 @@ function createFilterList(){
 	for(i in gameList){			
 		if( !unique[gameList[i].genre]){
 			distinct.push(gameList[i].genre);
-			unique[gameList[i].genre] = 1;			
-			console.log(distinct)
+			unique[gameList[i].genre] = 1;						
 			filters.innerHTML += `
 				<div class="checkbox">
-            		<label><input type="checkbox" class="icheck"> ` + gameList[i].genre + `</label>
+            		<label><input type="checkbox" class="icheck" id="` + gameList[i].genre + `"> ` + gameList[i].genre + `</label>
             	</div>`	
 		}
 	};
 }
 
-function applyFilter(){			
+function applyFilter(){	
+	let page = 1;
+	
 	fetchData(url)	
 		.then(data => {
 			gameList = data;	
-			createGameList();			
+			createGameList(page);			
 	});			
 }	
 
-document.getElementById('apply-btn').addEventListener('click', applyFilter)
+function onLoad(){
+	let page = 1;
+	
+	fetchData(url)	
+		.then(data => {
+			gameList = data;	
+			createFilterList();		
+			createGameList(page);			
+	});			
+}
+
+function sort(sort){
+	let page = 1;	
+
+	switch(sort){
+		case "alphabetical":
+			selectedSort.innerHTML = "Name";
+			break;
+		case "release-date":
+			selectedSort.innerHTML = "Release Date";
+			break;
+		case "popularity":
+			selectedSort.innerHTML = "Popularity";
+			break;
+		default:
+			selectedSort.innerHTML = "Order By";
+	}	
+
+	fetchData(url + "?sort-by=" + sort + "")	
+		.then(data => {
+			gameList = data;			
+			createGameList(page);			
+	});			
+}
+
+document.getElementById('apply-btn').addEventListener('click', applyFilter);
+document.getElementById('next_page').addEventListener('click', nextPage);
+document.getElementById('prev_page').addEventListener('click', prevPage);
+document.getElementById('name-sort').addEventListener('click', function(){sort("alphabetical")});
+document.getElementById('date-sort').addEventListener('click', function(){sort("release-date")});
+document.getElementById('pop-sort').addEventListener('click', function(){sort("popularity")});
